698f959054e7ce141dcae1e7dbabb21264390d64
rogervaldemar0 <rogervaldemar0@gmail.com>
2016-08-05 11:38:02 -0500

    update app

--- www/js/config.js
+++ www/js/config.js
@@ -3,6 +3,6 @@ var krms_config ={
 	'ApiUrl' : "YOUR_API_URL",
 	'DialogDefaultTitle' : "YOUR_OWN_DIALOG_TITLE",
 	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
-	'facebookAppId' : "YOUR_FACEBOOK_APP_ID",
+	'facebookAppId' : "1595558240736899",
 	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
 };
