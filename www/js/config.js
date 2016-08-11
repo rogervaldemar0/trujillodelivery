fe0346460b689adbec36edc3a22b8707e2c92f11
rogervaldemar0 <rogervaldemar0@gmail.com>
2016-08-10 20:56:59 -0500

    UPDATE CONFIG

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 
 var krms_config ={	
-	'ApiUrl' : "YOUR_API_URL",
-	'DialogDefaultTitle' : "YOUR_OWN_DIALOG_TITLE",
-	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
+	'ApiUrl' : "http://trujillodelivery.com/mobileapp/api",
+	'DialogDefaultTitle' : "Trujillo Delivery",
+	'pushNotificationSenderid' : "26768168976",
 	'facebookAppId' : "1595558240736899",
 	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
 };
