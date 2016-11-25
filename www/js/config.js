19db78666a87687ef4e86fd45f6fcf19a222057c
rogervaldemar0 <rogervaldemar0@gmail.com>
2016-11-24 19:48:55 -0500

    hash key

--- www/js/config.js
+++ www/js/config.js
@@ -4,5 +4,5 @@ var krms_config ={
 	'DialogDefaultTitle' : "Trujillo Delivery",
 	'pushNotificationSenderid' : "26768168976",
 	'facebookAppId' : "1595558240736899",
-	'APIHasKey' : "$2y$10$8IIyWww8.9D823nKzn4TmOmPB/VGJkeMhZ164VW.3DOHWqdcr1Fc2"
+	'APIHasKey' : "2jmj7l5rSw0yVb/vlWAYkK/YBwk="
 };
